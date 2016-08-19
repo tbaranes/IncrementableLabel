0e4001a520a28be9dddcab3a99fefdd547dfbab4
Tom Baranes <tom.baranes@gmail.com>
2016-08-19 21:05:59 +0200

    Run SwiftLint

--- Sources/IncrementableLabel.swift
+++ Sources/IncrementableLabel.swift
@@ -33,37 +33,37 @@ public typealias IncrementableLabelCompletion = () -> Void
 @IBDesignable public class IncrementableLabel: UILabel {
 
     // MARK: Properties
-    
+
     /// An options indicating how you want to perform the incrementation:
     public var option: IncrementableLabelOptions = .Linear
-    
+
     /// A callback closure which permits a greater control on how the text (attributed or not) is formatted between each incrementation.
     public var stringFormatter: StringFormatter?
     public var attributedTextFormatter: AttributedTextFormatter?
-    
+
     /// The rate used when an option is used.
     @IBInspectable public var easingRate: Double = 3.0
-    
+
     /// The format is used to set the text in the label. You can set the format to %f in order to display decimals.
     @IBInspectable public var format: String = "%d" {
         didSet {
             updateText()
         }
     }
-    
+
     // MARK: Private properties
-    
+
     fileprivate var timer: Timer?
     fileprivate var fromValue: Double = 0.0
     fileprivate var toValue: Double = 0.0
-    
+
     fileprivate var duration: TimeInterval = 0.3
     fileprivate var progress: TimeInterval = 0.0
     fileprivate var lastUpdate: TimeInterval = 0.0
     fileprivate var completion: IncrementableLabelCompletion?
-    
+
     // MARK: Getter
-    
+
     /** The label's value during the incrementation */
     public func currentValue() -> Double {
         if progress >= duration {
@@ -84,32 +84,32 @@ extension IncrementableLabel {
         self.completion = completion
         startIncrementation(fromValue: fromValue, toValue: toValue, duration: duration)
     }
-    
+
     /** Starts the incrementation from the current value to toValue */
     public func incrementFromCurrentValue(toValue: Double, duration: Double = 0.3, completion: IncrementableLabelCompletion? = nil) {
         self.completion = completion
         startIncrementation(fromValue: currentValue(), toValue: toValue, duration: duration)
     }
-    
+
     /** Starts the incrementation from zero to toValue */
     public func incrementFromZero(toValue: Double, duration: Double = 0.3, completion: IncrementableLabelCompletion? = nil) {
         self.completion = completion
         startIncrementation(fromValue: 0.0, toValue: toValue, duration: duration)
     }
-    
+
 }
 
-// MARK: - 
+// MARK: - Incrementation
 
 fileprivate extension IncrementableLabel {
-    
+
     func startIncrementation(fromValue: Double, toValue: Double, duration: Double) {
         self.fromValue = fromValue
         self.toValue = toValue
         self.duration = Double(duration)
         progress = 0
         lastUpdate = NSDate.timeIntervalSinceReferenceDate
-        
+
         self.timer?.invalidate()
         self.timer = nil
 
@@ -128,13 +128,13 @@ fileprivate extension IncrementableLabel {
             timer = nil
             progress = duration
         }
-        
+
         updateText()
         if progress == duration {
             completion?()
         }
     }
-    
+
     func updateText() {
         if let formatStringClosure = stringFormatter {
             text = formatStringClosure(currentValue())
@@ -149,15 +149,15 @@ fileprivate extension IncrementableLabel {
             }
         }
     }
-    
+
 }
 
 // MARK: Value helpers
 
 private extension IncrementableLabel {
-    
+
     // MARK: NextValue
-    
+
     func nextValueForCurrentOption(value: Double) -> Double {
         switch option {
         case .Linear: return nextValueForLinearOption(value: value)
@@ -166,19 +166,19 @@ private extension IncrementableLabel {
         case .EaseInOut: return nextValueForEaseInOutOption(value: value)
         }
     }
-    
+
     func nextValueForLinearOption(value: Double) -> Double {
         return value
     }
-    
+
     func nextValueForEaseInOption(value: Double) -> Double {
         return Double(powf(Float(value), Float(easingRate)))
     }
-    
+
     func nextValueForEaseOutOption(value: Double) -> Double {
         return 1.0 - Double(powf(1.0 - Float(value), Float(easingRate)))
     }
-    
+
     func nextValueForEaseInOutOption(value: Double) -> Double {
         var value = value
         let sign: Double = easingRate.truncatingRemainder(dividingBy: 2) == 0 ?  -1 : 1
